b01d23152a6324f44c7d9d5c7df98d9c4dd2b536
Paras Jain <85765189+parascode@users.noreply.github.com>
2023-07-02 19:08:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+function Counter() {
+  return <div>Hello</div>;
+}
